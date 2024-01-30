85110855e9789461a5fc7bf6149c3e1218331129
RmnDx <ramandeepkaur.dx@gmail.com>
2024-01-30 20:04:31 +0530

    first commit

---
+++ assets/useMail-gIBkDxeo.js
@@ -0,0 +1,1 @@
+import{e as n}from"./actions-Qz8gBtwW.js";import{an as i,aA as r}from"./index-umuunSGL.js";import{h as c}from"./selectors-KkM3saSA.js";function d({entity:s}){const{isLoading:t}=i(c),a=r();return{send:o=>{const e={id:o};a(n.mail({entity:s,jsonData:e}))},isLoading:t}}export{d as u};
